50d283a269c80823db4054df41a61bd4483207ef
Filipe Cabaco <filipe@talkdesk.com>
2016-03-29 14:40:02 +0100

    implemented keyword heuristic

---
+++ src/main/scala/org/bassethound/heuristic/impl/KeywordHeuristic.scala
@@ -0,0 +1,32 @@
+package org.bassethound.heuristic.impl
+
+import org.bassethound.heuristic.Heuristic
+
+class KeywordHeuristic extends Heuristic[String,Boolean]{
+  private final val Keywords = Set("key" , "secret" , "token")
+  private final val Separators = Set("=" , ":")
+
+  /*
+    Group 0 - Full match
+    Group 1 - Keyword found
+    Group 2 - Separator found
+    Group 3 - Word found (until next whitespace)
+   */
+  private final val ImportantRegex = s"""(${Keywords.mkString("|")})\\s*(${Separators.mkString("|")})\\s*(\\S+)""".r
+
+  /**
+    * Function responsible for asserting a score
+    *
+    * @param elem Element to be analysed
+    * @return Score asserted
+    */
+  override def analyseFunc(elem: String): Boolean = ImportantRegex.findAllMatchIn(elem).nonEmpty
+
+  /**
+    * Filter function to be applied by the heuristics filter
+    *
+    * @param result A tuple of Element / Score retrieved from analysis
+    * @return Is it a candidate?
+    */
+  override def filterFunc(result: (String, Boolean)): Boolean = result._2
+}
---
+++ src/test/scala/org/bassethound/heuristic/impl/KeywordHeuristicTest.scala
@@ -0,0 +1,34 @@
+package org.bassethound.heuristic.impl
+
+import org.scalatest.FunSuite
+import org.scalatest.Matchers._
+
+class KeywordHeuristicTest extends FunSuite {
+  private final val heuristic = new KeywordHeuristic()
+  test("test when keyword was found and contains relevant candidate") {
+    heuristic.analyseFunc("secret:pass") shouldBe true
+    heuristic.analyseFunc("key = pass") shouldBe true
+    heuristic.analyseFunc("key :pass") shouldBe true
+    heuristic.analyseFunc("token: pass") shouldBe true
+    heuristic.analyseFunc("token: pass //and then some comments") shouldBe true
+  }
+  
+  test("test when keyword exists and does not contains relevant candidate") {
+    heuristic.analyseFunc("token - not a secret") shouldBe false
+    heuristic.analyseFunc("token :") shouldBe false
+    heuristic.analyseFunc("token") shouldBe false
+  }
+
+  test("test that keyword was not found") {
+    heuristic.analyseFunc("this aren't the droids you're looking for") shouldBe false
+  }
+
+  test("test filter function for valid candidate") {
+    heuristic.filterFunc(("var = key",true)) shouldBe true
+  }
+
+  test("test filter function for non-candidate") {
+    heuristic.filterFunc(("no secrets here, move along",false)) shouldBe false
+  }
+
+}
