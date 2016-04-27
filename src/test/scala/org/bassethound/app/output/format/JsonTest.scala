13ea438cbf740b3849962b2048e4f3a4f8868b34
Filipe Cabaco <filipe@talkdesk.com>
2016-04-27 18:15:07 +0100

    removed unnecessary prints; improved gitignore

--- src/test/scala/org/bassethound/app/output/format/JsonTest.scala
+++ src/test/scala/org/bassethound/app/output/format/JsonTest.scala
@@ -6,13 +6,11 @@ import org.scalatest.Matchers._
 class JsonTest extends FunSuite {
   private val example = (1,Map( "source" -> (1,Map("heuristic" -> Seq(("candidate", 0 , "score"))))))
   test("test render") {
-    println(Json.render(example))
     Json.render(example) shouldBe
       """{"_1":1,"_2":{"source":{"_1":1,"_2":{"heuristic":[{"_1":"candidate","_2":0,"_3":"score"}]}}}}"""
   }
 
   test("test render pretty") {
-    println(Json.renderPretty(example))
     Json.renderPretty(example) shouldBe
       """{
         |  "_1":1,
